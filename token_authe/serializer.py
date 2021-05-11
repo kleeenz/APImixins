00467ad85bc6eee741de945a8f903edeec434816
PAKLEEENZ <clintonchristopher49@gmail.com>
2021-05-11 09:08:11 +0100

    secon_commit

--- token_authe/serializer.py
+++ token_authe/serializer.py
@@ -1,12 +1,12 @@
 from rest_framework import serializers
 from django.contrib.auth.models import User
 from .models import myMod
-from rest_framework.authtoken.models import Token
+
 
 
 class personSerial(serializers.ModelSerializer):
     myMods = serializers.PrimaryKeyRelatedField(many=True, queryset=myMod.objects.all())
-    person = serializers.ReadOnlyField(source='owner.username')
+    person = serializers.ReadOnlyField(source='person.username')
     class Meta:
         model = User
         fields = ['id', 'username', 'myMods', 'person']
--- token_authe/views.py
+++ token_authe/views.py
@@ -1,4 +1,6 @@
-from django.contrib.auth.models import User
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.authtoken.models import Token
+from rest_framework.response import Response
 from .serializer import myModserial, personSerial
 from .models import myMod
 from rest_framework import mixins
@@ -45,4 +47,14 @@ class persondetails(generics.RetrieveAPIView):
     queryset = myMod.objects.all()
     serializer_class = personSerial
 
+class CustomAuthToken(ObtainAuthToken):
+    def post(self, request, *args, **kwargs):
+        serializer = self.serializer_class(data=request.data,
+                                           context={'request': request})
+        serializer.is_valid(raise_exception=True)
+        user = serializer.validated_data['user']
+        token, created = Token.objects.get_or_created(user = user)
+        return Response({'token': token.key,
+                         'user.id': user.pk,
+                         'email': user.email})
 
